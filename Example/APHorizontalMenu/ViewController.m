6098391c0a9c250733fedf86fa2c08db3d6222ed
Abel Pascual <abelpascual@gmail.com>
2014-05-02 00:23:40 +0200

    Added GIF

--- Example/APHorizontalMenu/ViewController.m
+++ Example/APHorizontalMenu/ViewController.m
@@ -26,6 +26,10 @@
     //self.horizontalMenu.textSelectedColor = [UIColor blueColor];
     //self.horizontalMenu.selectedIndex = 2;
     //self.horizontalMenu.visibleItems = 3;
+    
+    APHorizontalMenu *menu2 = [[APHorizontalMenu alloc] initWithFrame:CGRectMake(0, 200, 320, 40)];
+    menu2.values = @[@"Item 1", @"Item 2", @"Item 3", @"Item 4", @"Item 5", @"Item 6", @"Item 7", @"Item 8", @"Item 9", @"Item 10"];
+    [self.view addSubview:menu2];
 }
 
 - (void)horizontalMenu:(id)horizontalMenu didSelectPosition:(NSInteger)index {
